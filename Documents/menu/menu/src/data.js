e39f281754da5935d6c277e160ebbe7fbd9b99e4
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2022-12-08 14:21:42 +0300

    Add Categories for the Different Menu Items

---
+++ Documents/menu/menu/src/data.js
@@ -0,0 +1,78 @@
+
+const menu = [
+  {
+    id: 1,
+    title: 'Butter Cookies',
+    category: 'Cookies',
+    price: 5000,
+    img: 'https://rasamalaysia.com/wp-content/uploads/2009/11/butter-cookies-thumb.jpg',
+    desc: `Butter cookies are made with butter as the main ingredient, which gives them their rich, buttery flavor and crisp, crumbly texture. `,
+  },
+  {
+    id: 2,
+    title: 'Fruit Cake',
+    category: 'Cake',
+    price: 15000,
+    img: 'https://merryboosters.com/wp-content/uploads/2019/12/Soft-Moist-FruitCake-500x500.jpg',
+    desc: `Our Clients enjoy fruit cake because of its rich, flavorful taste and texture, while others enjoy it for its festive appearance and association with special occasions. `,
+  },
+  {
+    id: 3,
+    title: 'chocolate chip cookies',
+    category: 'Cookies',
+    price: 10000,
+    img: 'https://img.taste.com.au/y3MeH6zH/taste/2016/11/chocolate-chip-cookies-23196-1.jpeg',
+    desc: `Chocolate chip cookies are a type of cookie that is made with chocolate chips or chunks as one of the main ingredients. The inclusion of chocolate in the cookies gives them a rich, sweet flavor and a satisfying texture. `,
+  },
+  {
+    id: 4,
+    title: 'Chocolate Cake',
+    category: 'Cake',
+    price: 25000,
+    img: 'https://thestayathomechef.com/wp-content/uploads/2016/06/The-Most-Amazing-Chocolate-Cake-2-e1598548411160.jpg',
+    desc: ` It's our most a popular option because of its rich, flavorful taste. Our clients enjoy chocolate cake because of its association with celebrations and special occasions, such as birthdays and anniversaries. `,
+  },
+
+  {
+    id: 5,
+    title: 'ginger cookies',
+    category: 'Cookies',
+    price: 6000,
+    img: 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2009/4/21/0/BX0122-1_Ultimate-Ginger-Cookie_s4x3.jpg.rend.hgtvcom.616.462.suffix/1383934532861.jpeg',
+    desc: `Ginger cookies are a type of cookie that is made with ginger, a spice that has a warm, slightly spicy flavor. Our Clients love ginger cookies because of their unique taste and aroma`,
+  },
+  {
+    id: 6,
+    title: 'sponge cake',
+    category: 'Cake',
+    price: 15000,
+    img: 'https://en.cooking-tree.com/wp-content/uploads/2022/03/Vanilla-Sponge-Cake-Genoise-Recipe-Cooking-tree-3.jpg',
+    desc: `It is a popular dessert option because of its light, airy texture and its versatility.   `,
+  },
+  {
+    id: 7,
+    title: 'Chicken Pizza',
+    category: 'Pizza',
+    price: 15000,
+    img: 'https://i.ndtvimg.com/i/2015-02/meat-pizza_625x350_41424681507.jpg',
+    desc: `It is a popular dessert option because of its light, airy texture and its versatility.   `,
+  },
+  {
+    id: 8,
+    title: 'Bacon Pizza',
+    category: 'Pizza',
+    price: 15000,
+    img: 'https://i.ndtvimg.com/i/2015-02/meat-pizza_625x350_41424681507.jpg',
+    desc: `It is a popular dessert option because of its light, airy texture and its versatility.   `,
+  },
+  {
+    id: 9,
+    title: 'Vegetable Pizza',
+    category: 'Pizza',
+    price: 10000,
+    img: 'https://i.ndtvimg.com/i/2015-02/meat-pizza_625x350_41424681507.jpg',
+    desc: `It is a popular dessert option because of its light, airy texture and its versatility.   `,
+  },
+
+];
+export default menu;

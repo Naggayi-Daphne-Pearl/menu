269e833f22c7f8de1b7fa54c3c3323864bc2aa33
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2022-12-10 11:42:03 +0300

    Render Navbar

--- Documents/menu/menu/src/App.js
+++ Documents/menu/menu/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from "react-router-dom"
 import Main from './Components/MenuItems/Main'
+import NavBar from './Components/NavBar/NavBar'
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <Main />
     </div>
   );
